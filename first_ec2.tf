4c37ad1d254065c943c9e31422036a7c5ce0f816
karan <karanbabars@gmail.com>
2025-01-21 11:24:11 +0530

    1 commit

---
+++ first_ec2.tf
@@ -0,0 +1,10 @@
+//configuration related to terraform.
+provider "aws" {
+  region     = "us-east-1"
+}
+
+//resource that we want to create.
+resource "aws_instance" "ec2" {
+  ami           = "ami-01816d07b1128cd2d"
+  instance_type = "t2.micro"
+}
