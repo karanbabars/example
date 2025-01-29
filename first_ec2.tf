77463208f04152ce8274f73844fa43c1f65b4821
Karan Sambhaji Babar <118063735+karanbabars@users.noreply.github.com>
2025-01-30 00:05:53 +0530

    Update first_ec2.tf

--- first_ec2.tf
+++ first_ec2.tf
@@ -1,14 +1,22 @@
-//configuration related to terraform.
+// AWS provider configuration
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
 }
 
-//resource that we want to create.
+// EC2 instance resource
 resource "aws_instance" "ec2" {
-  ami           = "ami-01816d07b1128cd2d"
+  ami           = "ami-01816d07b1128cd2d"  
   instance_type = "t2.micro"
-tags = {
+
+  tags = {
     Name = "MyEC2Instance20"
   }
-
 }
+
+// Output the public IP of the EC2 instance
+output "public_ip" {
+  value = aws_instance.ec2.public_ip
+}
+
+
+
