9b3a618ef37329fe1fae1e81fd169e7a0cdd3c4d
Karan Sambhaji Babar <118063735+karanbabars@users.noreply.github.com>
2025-02-07 07:10:25 +0530

    Update first_ec2.tf

--- first_ec2.tf
+++ first_ec2.tf
@@ -6,7 +6,7 @@ provider "aws" {
 // EC2 instance resource
 resource "aws_instance" "ec2" {
   ami           = "ami-01816d07b1128cd2d"  
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
 
   tags = {
     Name = "karan"
